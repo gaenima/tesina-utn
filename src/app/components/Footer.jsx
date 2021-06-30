8f663e3d3091d68984d5df22953ad0f532d451bb
Gaenima <gabriela.n.stocco@gmail.com>
2021-06-30 15:46:10 -0300

    más estilo

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
 return(
 <footer className="footer"> 
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
 <a className="navbar-brand" href="#"> </a>
 <div className="collapse navbar-collapse" id="navbarColor02">
   <ul className="navbar-nav m-auto">
